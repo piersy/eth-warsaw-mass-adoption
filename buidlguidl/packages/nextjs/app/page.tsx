9ecf3ebaf31e7e5921fffbe01c73c7d6f5a506d6
Paul Lange <palango@gmx.de>
2024-09-07 12:49:25 +0200

    Add entry using sepolia

--- buidlguidl/packages/nextjs/app/page.tsx
+++ buidlguidl/packages/nextjs/app/page.tsx
@@ -4,10 +4,14 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import { Address  } from "~~/components/scaffold-eth";
+import { AddressInput } from "./EnsEntry";
+import { useState } from "react";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+
+  const [address, setAddress] = useState("");
 
   return (
     <>
@@ -38,6 +42,8 @@ const Home: NextPage = () => {
             </code>
           </p>
         </div>
+
+        <AddressInput onChange={setAddress} value={address} placeholder="Input your address" />;
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
