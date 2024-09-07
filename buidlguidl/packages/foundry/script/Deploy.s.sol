4e71edf3fa7386c32768d47a450a08220aff28b5
Piers Powlesland <pierspowlesland@gmail.com>
2024-09-07 10:40:35 +0100

    Update gateway URL

--- buidlguidl/packages/foundry/script/Deploy.s.sol
+++ buidlguidl/packages/foundry/script/Deploy.s.sol
@@ -25,7 +25,7 @@ contract DeployScript is ScaffoldETHDeploy {
     );
 
     OffchainResolver offchainResolver = new OffchainResolver(
-      "https://api.yourdomain.com",
+      "http://svc-331-u22133.vm.elestio.app:8000",
       vm.addr(deployerPrivateKey)
     );
     console.logString(
