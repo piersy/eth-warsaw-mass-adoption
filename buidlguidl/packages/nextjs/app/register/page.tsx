98155d0db34e2e4293dd54ceadf76cddbbac47ca
Paul Lange <palango@gmx.de>
2024-09-07 14:03:37 +0200

    Improve form

--- buidlguidl/packages/nextjs/app/register/page.tsx
+++ buidlguidl/packages/nextjs/app/register/page.tsx
@@ -23,16 +23,19 @@ const Register: NextPage = () => {
         <div className="flex-grow bg-base-300 w-200 mt-16 px-8 py-12">
         <form className="space-y-4">
             <div>
-              <label htmlFor="phone" className="block text-sm font-medium">Phone number</label>
-              <input type="text" id="name" name="phone" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium">Email</label>
-              <input type="email" id="email" name="email" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-            </div>
-            <div>
               <label htmlFor="phone" className="block text-sm font-medium">Phone Number</label>
-              <input type="tel" id="phone" name="phone" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+              <input type="tel" id="phone" name="phone" value="12345" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="address" className="block text-sm font-medium">Address</label>
+              <input type="text" id="address" name="address" value="0x2C302520E6B344d8396BF3011862046287ef88c7" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="network" className="block text-sm font-medium">Network</label>
+              {/* <input type="text" id="network" name="network" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" /> */}
+              <select id="network" name="network">
+                <option value="op">OP</option>
+              </select>
             </div>
             <div>
               <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
